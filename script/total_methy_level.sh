e91c70aeca73fb11d93c8f5c35268ae9311147cc
Fu Xing <bio.xfu@gmail.com>
2018-12-12 15:49:46 +0800

    add cutoff parameter when calculating total methylation level

--- script/total_methy_level.sh
+++ script/total_methy_level.sh
@@ -1,3 +1,4 @@
 SAM=$1
+CUTOFF=$2
 
-cat dep4/${SAM}_dep4_Meth.txt|awk -v name="$SAM" '{if($5!=0) x+=1} END {print name"\t"x-1"\t"NR-1"\t"(x-1)/(NR-1)}'
+cat dep4/${SAM}_dep4_Meth.txt|awk -v name="$SAM" -v cutoff="$CUTOFF" '{if($7 > cutoff) x+=1} END {print name"\t"x-1"\t"NR-1"\t"(x-1)/(NR-1)}'
