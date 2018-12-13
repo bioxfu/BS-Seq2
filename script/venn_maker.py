a1487be9a537af0eb9fe8d1f8702858b7ab2eae6
Fu Xing <bio.xfu@gmail.com>
2018-12-13 13:55:26 +0800

    save image as png

--- script/venn_maker.py
+++ script/venn_maker.py
@@ -8,9 +8,9 @@ output = sys.argv[3]
 #names = ['ros1_4', 'suvh13', 'suvh1378']
 
 args = {}
-args['2'] = ['col="transparent"', 'fill=c("cornflowerblue","green")', 'alpha=0.50', 'cex=1.5', 'cat.cex=1.5', 'euler.d=TRUE', 'scaled=TRUE', 'cat.col=c("darkblue","darkgreen")']
-args['3'] = ['col="transparent"', 'fill=c("cornflowerblue","green","yellow")', 'alpha=0.50', 'cex=1.5', 'cat.cex=1.5', 'euler.d=TRUE', 'scaled=TRUE', 'cat.col=c("darkblue","darkgreen","orange")']
-args['4'] = ['col="transparent"', 'fill=c("cornflowerblue","green","yellow","darkorchid1")', 'alpha=0.50', 'cex=1.5', 'cat.cex=1.5', 'euler.d=TRUE', 'scaled=TRUE', 'cat.col=c("darkblue","darkgreen","orange","darkorchid4")', 'label.col=c("orange","white","darkorchid4","white","white","white","white","white","darkblue","white","white","white","white","darkgreen","white")']
+args['2'] = ['imagetype="png"', 'col="transparent"', 'fill=c("cornflowerblue","green")', 'alpha=0.50', 'cex=1.5', 'cat.cex=1.5', 'euler.d=TRUE', 'scaled=TRUE', 'cat.col=c("darkblue","darkgreen")']
+args['3'] = ['imagetype="png"', 'col="transparent"', 'fill=c("cornflowerblue","green","yellow")', 'alpha=0.50', 'cex=1.5', 'cat.cex=1.5', 'euler.d=TRUE', 'scaled=TRUE', 'cat.col=c("darkblue","darkgreen","orange")']
+args['4'] = ['imagetype="png"', 'col="transparent"', 'fill=c("cornflowerblue","green","yellow","darkorchid1")', 'alpha=0.50', 'cex=1.5', 'cat.cex=1.5', 'euler.d=TRUE', 'scaled=TRUE', 'cat.col=c("darkblue","darkgreen","orange","darkorchid4")', 'label.col=c("orange","white","darkorchid4","white","white","white","white","white","darkblue","white","white","white","white","darkgreen","white")']
 
 pybedtools.contrib.venn_maker.venn_maker(beds, names=names, figure_filename=output, script_filename='venn.R', additional_args=args[str(len(beds))], run=True)
 
